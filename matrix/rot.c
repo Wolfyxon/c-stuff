cd44e09e80a1435c9738a299ae4f50b7f522355e
Wolfyxon <wolfyxon@gmail.com>
2024-11-18 17:42:09 +0100

    Init matrix rotation

---
+++ matrix/rot.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+const size_t SIZE = 3;
+const size_t LEN = SIZE * SIZE;
+
+int main(void) {
+    int matrix[LEN];
+
+    printf("Enter a %ix%i matrix:\n", SIZE);
+
+    for(int i = 0; i < SIZE; i++) {
+        scanf("%i %i %i", &matrix[i] + 0, &matrix[i] + 1, &matrix[i] + 2);
+    }
+
+    puts("Rotated 90* clockwise:");
+
+
+
+    puts("");
+}
