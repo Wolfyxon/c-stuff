e120b7785aab15a1eafd54e4535901501379a118
Wolfyxon <wolfyxon@gmail.com>
2024-11-19 20:24:55 +0100

    Map display

--- matrix/movement.c
+++ matrix/movement.c
@@ -2,9 +2,14 @@
 #include <curses.h> // Make sure to include the -lncurses flag
 
 const int SIZE = 8;
+const int LEN = SIZE * SIZE;
 
 int main(void) {    
-    int map[SIZE * SIZE];
+    int map[LEN];
+    
+    for(int i = 0; i < LEN; i++) {
+        map[i] = 0;
+    }
 
     int x = SIZE / 2;
     int y = SIZE / 2;
@@ -14,8 +19,22 @@ int main(void) {
 
         addstr("Controls:\n");
         addstr("  wasd - move\n");
-        addstr("  x    - exit\n");
+        addstr("  x    - exit\n\n");
         
+        for(int iy = 0; iy < SIZE; iy++) {
+            for(int ix = 0; ix < SIZE; ix++) {
+                int cell = map[(iy * SIZE) + ix];
+                
+                char ch = '0';
+                if(cell) ch = '1';
+                
+                addstr(&ch);
+                addstr(" ");
+            }
+
+            addstr("\n");
+        }
+
         char inp = getch();
         
         endwin();
