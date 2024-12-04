781a6af9210c72f079173299701aa9612cf43c74
Wolfyxon <wolfyxon@gmail.com>
2024-12-04 16:49:42 +0100

    Init chemistry

---
+++ chem/hydrocarbons/which.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+
+alkane (PL: alkan):
+    C_{n} H_{2n + 2}
+
+alkene (PL: alken):
+    C_{n} H_{2n}
+
+alkyne (PL: alkin):
+    C_{n} H_{2n-2}
+
+*/
+
+int main(void) {
+
+}
