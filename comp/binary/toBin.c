6ec3dd710e721fcf2f8ad1c512ff7f5aa4eb7483
Wolfyxon <wolfyxon@gmail.com>
2025-02-02 15:28:48 +0100

    Added toBin

---
+++ comp/binary/toBin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        puts("Usage: <decimal number>");
+        return 1;
+    }
+
+    int decimal = atoi(argv[1]);
+    
+    int len = 0;
+    int binary[64];
+
+    for(len; decimal; len++) {
+        binary[len] = decimal % 2;
+        decimal /= 2;
+    }
+
+    for(int i = 0; i < len; i++) {
+        printf("%i", binary[len - i - 1]);
+    }
+
+    puts("");
+    
+}
